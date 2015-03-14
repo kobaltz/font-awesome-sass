2f3db41a9ad731048c2057ebc309cf6cf3055816
kobaltz <dave@k-innovations.net>
2015-03-14 01:26:31 -0400

    Update helpers.rb

--- lib/font_awesome/sass/rails/helpers.rb
+++ lib/font_awesome/sass/rails/helpers.rb
@@ -4,8 +4,8 @@ module FontAwesome
       module ViewHelpers
         def icon(icon, *args)
           text, html_options = args
-          html_options = text if text.is_a?(Hash)
-          
+          text.is_a?(Hash) ? html_options = text : html_options = {}
+
           content_class = "fa fa-#{icon}"
           content_class << " #{html_options[:class]}" if html_options.key?(:class)
           html_options[:class] = content_class
